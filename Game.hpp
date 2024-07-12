72831714c626dd5417d22da7334e8c82710dc284
Sokrat1109 <146949643+Sokrat1109@users.noreply.github.com>
2024-07-12 11:42:11 +0200

    Update Game.hpp

--- Game.hpp
+++ Game.hpp
@@ -5,11 +5,11 @@
 #include <windows.h>
 #include <time.h>
 
-#include "Ship.hpp"
-#include "Bullet.hpp"
+#include "Meditation.hpp"
+#include "Note.hpp"
 #include "Timer.hpp"
 #include "Control.hpp"
-#include "Circle.hpp"
+#include "Dairy.hpp"
 #include "ControlTime.hpp"
 
 #include <SFML/Graphics.hpp>
@@ -31,14 +31,28 @@ namespace mt  // пространство имен
 		sf::Texture MeditationT, DairyT, ControlT;
 		sf::Sprite MeditationS, DairyS, ControlS;
 		
-		sf::Font font;
-		sf::Text text;
+		sf::Font m_font;
+		sf::Text m_text;
 
-		Dairy launch_dairy;
-		Meditation launch_meditation;
-		Control launch_control;
-		
+		Dairy* launch_dairy;
+		Meditation* launch_meditation;
+		Control* launch_control;
+		Note* launch_note;
+		Timer* launch_timer;
+		Controltime* launch_controltime;
 
+		int m_state = 0;
+
+		sf::Sprite m_backgroundSprite;
+		sf::Texture m_backgroundTexture;
+
+		const int MAIN_WINDOW = 0;
+		const int DAIRY = 1;
+		const int MEDITATION = 2;
+		const int CONTROL = 3;
+		const int NOTE = 11;
+		const int TIMER = 21;
+		const int CONTROLTIME = 31;
 
 
 	public:
@@ -48,6 +62,23 @@ namespace mt  // пространство имен
 			m_width = width;
 			m_height = height;
 			m_capture = capture;
+
+			launch_dairy = new Dairy(m_width, m_height);
+			launch_meditation = new Meditation(m_width, m_height); // m_capture
+			launch_control = new Control(m_width, m_height);
+			launch_note = new Note(m_width, m_height);
+			launch_timer = new Timer(m_width, m_height);
+			launch_controltime = new Controltime(m_width, m_height);
+		}
+
+		~Game()
+		{
+			delete launch_dairy;
+			delete launch_meditation;
+			delete launch_control;
+			delete launch_timer;
+			delete launch_note;
+			delete launch_controltime;
 		}
 
 		void Setup(int n)
@@ -69,9 +100,42 @@ namespace mt  // пространство имен
 			ControlS.setTexture(ControlT);
 			ControlS.setPosition(1400, 350);
 
-			sf::Text text("Welcome", font, 20);
-			font.loadFromFile("arial.ttf");
-			text.setColor(sf::Color::Black);
+			if (!m_font.loadFromFile("arial.ttf"))
+			{
+				std::cerr << "Error in font loading" << std::endl;
+			}
+
+			m_text.setFont(m_font);
+			m_text.setCharacterSize(20);
+			m_text.setFillColor(sf::Color::Black);
+			// НАПЕЧАТАТЬ САМ ТЕКСТ
+
+			
+			if (!m_backgroundTexture.loadFromFile("kaplya.png")) {
+				std::cerr << "Error in loading background image" << std::endl;
+				return; // ошибка загрузки картинки
+			}
+			m_backgroundSprite.setTexture(m_backgroundTexture);
+
+			sf::Vector2f targetSize(m_width, m_height); //целевой размер
+
+			m_backgroundSprite.setScale(
+				targetSize.x / m_backgroundSprite.getLocalBounds().width,
+				targetSize.y / m_backgroundSprite.getLocalBounds().height);
+
+			sf::Vector2f targetSizeS(400.0f, 240.0f); //целевой размер
+
+			MeditationS.setScale(
+				targetSizeS.x / MeditationS.getLocalBounds().width,
+				targetSizeS.y / MeditationS.getLocalBounds().height);
+
+			DairyS.setScale(
+				targetSizeS.x / DairyS.getLocalBounds().width,
+				targetSizeS.y / DairyS.getLocalBounds().height);
+
+			ControlS.setScale(
+				targetSizeS.x / ControlS.getLocalBounds().width,
+				targetSizeS.y / ControlS.getLocalBounds().height);
 
 		}
 
@@ -81,72 +145,120 @@ namespace mt  // пространство имен
 
 			while (m_window.isOpen())
 			{
-				sf::Texture backgroundTexture;
-				if (!backgroundTexture.loadFromFile("kaplya.png")) {
-					return; // ошибка загрузки картинки
-				}
-
-				sf::Sprite backgroundSprite;
-				backgroundSprite.setTexture(backgroundTexture);
-
-				sf::Vector2f targetSize(m_width, m_height); //целевой размер
-
-				backgroundSprite.setScale(
-					targetSize.x / backgroundSprite.getLocalBounds().width,
-					targetSize.y / backgroundSprite.getLocalBounds().height);
-
-				sf::Vector2f targetSizeS(400.0f, 240.0f); //целевой размер
-
-				MeditationS.setScale(
-					targetSizeS.x / MeditationS.getLocalBounds().width,
-					targetSizeS.y / MeditationS.getLocalBounds().height);
-
-				DairyS.setScale(
-					targetSizeS.x / DairyS.getLocalBounds().width,
-					targetSizeS.y / DairyS.getLocalBounds().height);
-
-				ControlS.setScale(
-					targetSizeS.x / ControlS.getLocalBounds().width,
-					targetSizeS.y / ControlS.getLocalBounds().height);
-
-				
-
 				sf::Event event;
 				while (m_window.pollEvent(event))
 				{
 					if (event.type == sf::Event::Closed)
+					{
 						// сохранить результат работы в приложении
 						m_window.close();
-
-					if (event.type == sf::Event::MouseWheelMoved)
-					{
-						event.mouseWheel.delta;
-						event.mouseWheel.x;
-						event.mouseWheel.y;
 					}
-
 
 					if (event.type == sf::Event::MouseButtonPressed)
 					{
-						if (event.type == sf::Event::MouseMoved)
+						int x = sf::Mouse::getPosition(m_window).x;
+						int y = sf::Mouse::getPosition(m_window).y;
+
+						if (m_state == MAIN_WINDOW)
 						{
-							if (event.mouseButton.button == sf::Mouse::Left)// левая кнопка мыши нажата
+							std::cout << x << " " << y << std::endl;
+							if ((x > 200 && x < 500) && (y > 430 && y < 530)) //дневник
 							{
-								if ((event.mouseMove.x > 150 & event.mouseMove.x < 170) &(event.mouseMove.y > 350 & event.mouseMove.y < 360)) // координаты на кнопке
-								{
-									launch_dairy.LifeCycleDairy();
-								}
-								if ((event.mouseMove.x > 770 & event.mouseMove.x < 790) & (event.mouseMove.y > 470 & event.mouseMove.y < 480)) // координаты на кнопке
-								{
-									launch_meditation.LifeCycleMeditation();
-								}
-								if ((event.mouseMove.x > 1400 & event.mouseMove.x < 1420) & (event.mouseMove.y > 350 & event.mouseMove.y < 360)) // координаты на кнопке
-								{
-									launch_control.LifeCycleControl();
-								}
+								std::cout << "1" << std::endl;
+								m_state = DAIRY;
+							}
+							if ((x > 820 && x < 1120) && (y > 550 && y < 650)) // медитация
+							{
+								std::cout << "1" << std::endl;
+								m_state = MEDITATION;
+							}
+							if ((x > 1450 && x < 1750) && (y > 430 && y < 530)) //дневник
+							{
+								std::cout << "1" << std::endl;
+								m_state = CONTROL;
+							}
+						}
+						else if (m_state == DAIRY)
+						{
+							// ПЕРЕДЕЛАТЬ КООРДИНАТЫ
+							std::cout << x << " " << y << std::endl;
+							if ((x > 0 && x < 200) && (y > 0 && y < 200)) // назад
+							{
+								std::cout << "1" << std::endl;
+								m_state = MAIN_WINDOW;
+							}
+							// ПЕРЕДЕЛАТЬ КООРДИНАТЫ
+							if ((x > 0 && x < 200) && (y > 550 && y < 650)) // записка
+							{
+								std::cout << "1" << std::endl;
+								m_state = NOTE;
+							}
+						}
+						else if (m_state == MEDITATION)
+						{
+							// ПЕРЕДЕЛАТЬ КООРДИНАТЫ
+							std::cout << x << " " << y << std::endl;
+							if ((x > 0 && x < 200) && (y > 0 && y < 200)) // назад
+							{
+								std::cout << "1" << std::endl;
+								m_state = MAIN_WINDOW;
+							}
+							// ПЕРЕДЕЛАТЬ КООРДИНАТЫ
+							if ((x > 820 && x < 1120) && (y > 550 && y < 650)) // таймер старт
+							{
+								std::cout << "1" << std::endl;
+								m_state = TIMER;
+							}
+						}
+						else if (m_state == CONTROL)
+						{
+							// ПЕРЕДЕЛАТЬ КООРДИНАТЫ
+							std::cout << x << " " << y << std::endl;
+							if ((x > 20 && x < 100) && (y > 20 && y < 100)) // назад
+							{
+								std::cout << "1" << std::endl;
+								m_state = MAIN_WINDOW;
+							}
+							// ПЕРЕДЕЛАТЬ КООРДИНАТЫ
+							if ((x > 820 && x < 1120) && (y > 550 && y < 650)) // дыхание старт
+							{
+								std::cout << "1" << std::endl;
+								m_state = CONTROLTIME;
+							}
+						}
+						else if (m_state == CONTROLTIME)
+						{
+							// ПЕРЕДЕЛАТЬ КООРДИНАТЫ
+							std::cout << x << " " << y << std::endl;
+							if ((x > 20 && x < 100) && (y > 20 && y < 100)) // назад
+							{
+								std::cout << "1" << std::endl;
+								m_state = CONTROL;
+							}
+						}
+						else if (m_state == TIMER)
+						{
+							// ПЕРЕДЕЛАТЬ КООРДИНАТЫ
+							std::cout << x << " " << y << std::endl;
+							if ((x > 20 && x < 100) && (y > 20 && y < 100)) // назад
+							{
+								std::cout << "1" << std::endl;
+								m_state = MEDITATION;
+							}
+						}
+						else if (m_state == NOTE)
+						{
+							// ПЕРЕДЕЛАТЬ КООРДИНАТЫ
+							std::cout << x << " " << y << std::endl;
+							if ((x > 20 && x < 100) && (y > 20 && y < 100)) // назад
+							{
+								std::cout << "1" << std::endl;
+								m_state = DAIRY;
 							}
 						}
 					}
+						
+					
 				}
 
 
@@ -154,13 +266,40 @@ namespace mt  // пространство имен
 				clock.restart();
 
 				m_window.clear();
-				m_window.draw(backgroundSprite);
 
-				m_window.draw(text);
+				if (m_state == MAIN_WINDOW)
+				{
+					m_window.draw(m_backgroundSprite);
+					m_window.draw(m_text);
+					m_window.draw(MeditationS);
+					m_window.draw(DairyS);
+					m_window.draw(ControlS);
+				}
+				else if (m_state == DAIRY)
+				{
+					launch_dairy->LifeCycleDairy(m_window);
+				}
+				else if (m_state == MEDITATION)
+				{
+					launch_meditation->LifeCycleMeditation(m_window);
+				}
+				else if (m_state == CONTROL)
+				{
+					launch_control->LifeCycleControl(m_window);
+				}
+				else if (m_state == NOTE)
+				{
+					launch_note->LifeCycleNote(m_window);
+				}
+				else if (m_state == TIMER)
+				{
+					launch_timer->LifeCycleTimer(m_window);
+				}
+				else if (m_state == CONTROLTIME)
+				{
+					launch_controltime->LifeCycleControltime(m_window);
+				}
 
-				m_window.draw(MeditationS);
-				m_window.draw(DairyS);
-				m_window.draw(ControlS);
 				m_window.display();
 			}
 		}
